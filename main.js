d14cf6465bb9e86cca192dad0cd6d6d1577d4e50
Diyasanthosh97 <91463964+Diyasanthosh97@users.noreply.github.com>
2022-04-08 19:21:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+Webcam.set({
+    width:350,
+    height:350,
+    image_format:"jpg",
+    jpg_quality:100
+});
+camera=document.getElementById("camera");
+Webcam.attach("#camera");
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="capture_img" src="'+data_uri+'"/>';
+    });
+
+}
+console.log("ml5-version",ml5.version);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/rFN6wsGX4/model.json",modelLoaded);
+function modelLoaded(){
+    console.log("modal loaded");
+}
